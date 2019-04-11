d2de23e64381af043c99065f95cfb45a56c4fb09
Allan Camargo <allan.cdev@gmail.com>
2019-04-11 04:13:26 -0300

    Alterando camninho do mongo

--- src/server.js
+++ src/server.js
@@ -17,7 +17,8 @@ io.on('connection', socket => {
     });
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/db_omni',
+const conn = "mongodb+srv://admin:admin@cluster0-pxsfz.mongodb.net/test?retryWrites=true";
+mongoose.connect(conn,
     { useNewUrlParser: true }
 );
 
